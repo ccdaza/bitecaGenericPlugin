dfecf4c402b57c48ababf1c446164bc21a9eb619
ccdaza <ccdaza2@gmail.com>
2019-06-25 11:55:05 -0500

    fix, not set data json

--- src/BitecaPluginForm.php
+++ src/BitecaPluginForm.php
@@ -3,11 +3,11 @@ namespace BitecaGenericPlugin;
 
 import('lib.pkp.classes.form.Form');
 class BitecaPluginForm extends \Form {
-	
+
     public $_journalId;
     public $_plugin;
     public $data;
-	
+
     function __construct($plugin, $journalId) {
         $this->_journalId = $journalId;
         $this->_plugin = $plugin;
@@ -26,27 +26,27 @@ class BitecaPluginForm extends \Form {
             'token' => $this->_plugin->getSetting($this->_journalId, 'token'),
             'client_id' => $this->_plugin->getSetting($this->_journalId, 'client_id')
         ];
-      
+
         foreach($this->data as $key => $data){
-            if(!isset($data[$key]["value"])){
+            if(!isset($data["value"])){
                 $this->_data[$key] = $this->_plugin->getSetting($this->_journalId, $key);
             }else{
-                $this->_data[$key] = $data->value;
+                $this->_data[$key] = $data["value"];
             }
         }
-			
+
     }
     /**
      * Assign form data to user-submitted data.
      */
     function readInputData() {
-			$read = ['token', 'client_id'];
-      
-			foreach($this->data as $key => $data){
-					$read[] = $key;
-				}
-			
-			$this->readUserVars($read);
+        $read = ['token', 'client_id'];
+
+        foreach($this->data as $key => $data){
+            $read[] = $key;
+        }
+
+        $this->readUserVars($read);
     }
     /**
      * Fetch the form.
@@ -63,40 +63,39 @@ class BitecaPluginForm extends \Form {
     function execute() {
         $this->_plugin->updateSetting($this->_journalId, 'token', trim($this->getData('token'), "\"\';"), 'string');
         $this->_plugin->updateSetting($this->_journalId, 'client_id', trim($this->getData('client_id'), "\"\';"), 'string');
-			
-				foreach($this->data as $key => $data){
-					
-					$function = "clear".ucfirst($data["type"]);
-					
-					if(method_exists($this, $function)){
-						$value = $this->$function($this->getData($key));
-					}else{
-						$value = $this->getData($key);
-					}
 
-					$this->_updateSetting($key, $this->getData($key), $data["type"]);
-					
-				}
+        foreach($this->data as $key => $data){
+            if(!isset($data["value"])){
+                $function = "clear".ucfirst($data["type"]);
+                if(method_exists($this, $function)){
+                    $value = $this->$function($this->getData($key));
+                }else{
+                    $value = $this->getData($key);
+                }
+
+                $this->_updateSetting($key, $value, $data["type"]);
+            }
+        }
     }
-	
-		function clearString($value){
-			return trim($value, "\"\';");
-		}
-	
-		function clearInt($value){
-			return intval($value);
-		}
-	
-		function clearBool($value){
-			return $value == "on";
-		}
-			
-	
-		function clearJson($value){
-			return json_encode($value);
-		}
-	
-		function _updateSetting($key, $value, $type){
-			$this->_plugin->updateSetting($this->_journalId, $key, $value, $type);
-		}
+
+    function clearString($value){
+        return trim($value, "\"\';");
+    }
+
+    function clearInt($value){
+        return intval($value);
+    }
+
+    function clearBool($value){
+        return $value == "on";
+    }
+
+
+    function clearJson($value){
+        return json_encode($value);
+    }
+
+    function _updateSetting($key, $value, $type){
+        $this->_plugin->updateSetting($this->_journalId, $key, $value, $type);
+    }
 }
